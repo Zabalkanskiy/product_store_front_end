ad03079d7e0fa12453f72f62905d34f211f10063
Alexander <azabalkanskij@hotmail.com>
2019-04-27 13:50:59 +0300

    error

--- src/model.rs
+++ src/model.rs
@@ -1,80 +1,86 @@
 
-pub struct Product{
-    id: Option<usize>,
-    name: Option<&str>,
-    category:Option<&str>,
-    descriptions: Option<&str>,
-    price: Option<usize>,
+pub struct Product<'a>{
+id: Option<usize>,
+name: Option<&'a str>,
+category:Option<&'a str>,
+descriptions: Option<&'a str>,
+price: Option<usize>,
 }
 
-impl Product{
-    pub fn new()-> Self{
-        Product{
-            id: None,
-            name: None,
-            category: None,
-            descriptions: None,
-            price: None
-        }
+impl<'a> Product<'a>{
+pub fn new()-> Self{
+    Product{
+        id: None,
+        name: None,
+        category: None,
+        descriptions: None,
+        price: None,
     }
 }
-
-pub struct ProductRepositiry{
-    products : Box<Vec<Product>>,
-    categories : Vec<String>,
 }
 
-impl ProductRepositiry{
-    pub fn getProducts(&self, category: Option< &str>)->Box<Vec<Product>>{
-       let result  = self.products.into_iter().filter(|&p : &Product|{
-          if  p.category == None || p.category == category {
-              true
-          } else {
-              false
-          } 
-
-        });
-
-        result
-    } 
-
-    pub fn getProduct(&self, id: Option<usize>) ->  Product {
-      let res = self.products.into_iter().find(|&p|{ if p.id == id{true}else {false}})
-    }
-
-    pub fn getCategories(&self) -> Vec<String>{
-        self.categories
-    }
+pub struct ProductRepositiry<'a>{
+products : Box<Vec<Product<'a>>>,
+categories : Vec<String>,
 }
 
-pub struct   Cart{
-    lines: Vec<CartLine>,
-    item_count : usize,
-    cart_price : usize,
+impl<'a> ProductRepositiry<'a>{
+pub fn getProducts(&self, category: Option< &str>)->Box<Vec<Product>>{
+    let result : Vec<_>  = self.products.into_iter().filter(|&p : &Product|{
+        if  p.category == None || p.category == category {
+            true
+        } else {
+            false
+        } 
 
+    }).collect();
+
+    Box::new(result)
+} 
+
+pub fn getProduct(&self, id: Option<usize>) -> Option<Product> {
+    let res: Option<Product> = self.products.into_iter().find(|&p| p.id == id);
+    res
 }
 
-impl Cart {
+pub fn getCategories(&self) -> Vec<String>{
+    self.categories
+}
+}
 
-    pub fn addLine(&self, product : Product, quantity : i32   ){
-      let result = self.lines.find(|line|-> Cartline {line.product.id == product.id} );
-        if result != None {
-            result.quantity += quantity
-        }else {
-            self.lines.push ( CartLine{product, quantity})
-        }
-        self.recalculate
-    }
+pub struct   Cart<'a>{
+lines: Vec<CartLine<'a>>,
+item_count : usize,
+cart_price : usize,
+quantity   : usize
+}
 
-  fn  recalculate(&self){
+impl<'a> Cart<'a> {
+//where
 
+pub fn addLine(&self, product : Product, quantity : i32   )->Self{
+    let result = self.lines.iter().find(|&line  | line.product.id == product.id );
+    if let Some(result) = result.take() {
+        self.lines.push ( CartLine <'a> {product<'a>, quantity})
+    }else {
         unimplemented!();
-        
+        //result.quantity += quantity
     }
+    self.recalculate()
 }
 
-pub struct CartLine{
-   product : Product,
-   quantity : i32
+fn  recalculate(&mut self)-> Self{
+
+    unimplemented!();
+    
+}
 }
 
+pub struct CartLine<'a>{
+product : Product<'a>,
+quantity : i32
+}
+
+fn filter_one(input: &[u8]) -> impl Iterator<Item = &u8> {
+input.iter().filter(|&&x| x == 1)
+}
